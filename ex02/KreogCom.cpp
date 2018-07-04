44f1ca692bedf249379cf400906992c1f9df63c1
17150012 <17150012@bjtu.edu.cn>
2018-07-04 19:11:09 +0800

    second commit

--- ex02/KreogCom.cpp
+++ ex02/KreogCom.cpp
@@ -1,60 +1,58 @@
-#include <iostream>
-#include <string>
 #include "KreogCom.h"
 
-KreogCom::KreogCom(int x, int y, int serial)
+KreogCom::KreogCom(int x, int y, int serial) :
+    _next(NULL), _x(x), _y(y), m_serial(serial)
 {
-	this->my_x = x;
-	this->my_y = y;
-	this->m_serial = serial;
-	this->next = NULL;
-	std::cout << "KreogCom " << serial << " initialised" << std::endl;
+    std::cout << "KreogCom " << m_serial << " initialised" << std::endl;
 }
 
 KreogCom::~KreogCom()
 {
-	std::cout << "KreogCom " << this->m_serial << " shutting down" << std::endl;
+    std::cout << "KreogCom " << m_serial << " shutting down" << std::endl;
 }
 
 void KreogCom::addCom(int x, int y, int serial)
 {
-	KreogCom *elem = new KreogCom(x, y, serial);
-
-	elem->next = this->next;
-	this->next = elem;
+    KreogCom* elem = new KreogCom(x, y, serial);
+    if (!_next)
+        _next = elem;
+    else
+    {
+        elem->_next = _next;
+        _next = elem;
+    }
 }
 
-KreogCom	*KreogCom::getCom()
+KreogCom* KreogCom::getCom()
 {
-	return this->next;
+    return _next;
 }
 
 void KreogCom::removeCom()
 {
-	KreogCom	*elem;
-
-	if (this->next)
-	{
-		elem = this->next;
-		this->next = this->next->next;
-		delete elem;
-	}
+    if (!_next)
+        return;
+    if (_next->_next)
+    {
+        KreogCom* tmp = _next;
+        _next = _next->_next;
+        delete tmp;
+    }
+    else
+    {
+        delete _next;
+        _next = NULL;
+    }
 }
 
 void KreogCom::ping() const
 {
-	//if (this)
-		std::cout << "KreogCom " << this->m_serial << " currently at " << this->my_x << " " << this->my_y  << std::endl;
+    std::cout << "KreogCom " << m_serial << " currently at " << _x << " " << _y << std::endl;
 }
 
 void KreogCom::locateSquad() const
 {
-	KreogCom *tmp = this->next;
-
-	while (tmp)
-	{
-		tmp->ping();
-		tmp = tmp->next;
-	}
-	ping();
+    for (KreogCom* itr = _next; itr != NULL; itr = itr->getCom())
+        itr->ping();
+    ping();
 }
--- ex02/KreogCom.h
+++ ex02/KreogCom.h
@@ -1,25 +1,26 @@
 #ifndef KREOGCOM_H_
 #define KREOGCOM_H_
 
-#include <string>
 #include <iostream>
 
 class KreogCom
 {
-	int m_serial;
-	int my_x;
-	int my_y;
-	KreogCom	*next;
+public:
+    KreogCom(int x, int y, int serial);
+    ~KreogCom();
 
-	public:
-		KreogCom(int x, int y, int serial);
-		~KreogCom();
+    void addCom(int x, int y, int serial);
+    KreogCom* getCom();
+    void removeCom();
 
-	void addCom(int x, int y, int serial);
-	KreogCom	*getCom();
-	void removeCom();
-	void ping() const;
-	void locateSquad() const;
+    void ping() const;
+    void locateSquad() const;
+
+private:
+    KreogCom* _next;
+    int _x;
+    int _y;
+    const int m_serial;
 };
 
 #endif
